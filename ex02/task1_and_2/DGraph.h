9efc5886c6218d6f776abb322ca85b9b88bce86c
Alexander Lochmann <A.Lochmann@sudent.uibk.ac.at>
2017-10-25 18:12:12 +0200

    added header file

---
+++ ex02/task1_and_2/DGraph.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+//adjacent list
+class DGraph : std::vector<std::vector<unsigned>> {
+public:
+	DGraph();
+
+	unsigned add_vertex();
+	void add_edge(unsigned outgoing_node, unsigned incoming_node);
+
+	std::vector<unsigned> vertex_out_degree(unsigned degree);
+
+	friend std::ostream &operator<<(std::ostream &os, const DGraph &m);
+	//TODO
+	void serialize(std::ofstream& f) ;
+	static DGraph deserialize(std::istream& s);
+	unsigned root_node;
+};
+
+std::ostream &operator<<(std::ostream &os, const DGraph &m);
