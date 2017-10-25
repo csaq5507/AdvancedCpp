b2e7d92449a073804a387f90efa8e3d65de43d05
Alexander Lochmann <A.Lochmann@sudent.uibk.ac.at>
2017-10-25 15:11:11 +0200

    created test for file createion

--- ex02/task1_and_2/Source.cpp
+++ ex02/task1_and_2/Source.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <experimental/filesystem>
 #include <string_view>
-#include "vcs_utility.h"
+#include <fstream>
 
 using namespace std;
 namespace fs = experimental::filesystem;
 
 int main(int argv, char** args) {
-	/*
+	
 	auto path = fs::current_path();
-	if (!is_vcs_initialized(path)) {
-		init_vcs(path);
-	}
-	call_status(path, getStagedFileFromRootDic(path));
-	getchar();
-	*/
+	cout << path << endl;
+	ofstream file(path / "t" / "t.txt");
+	file << "lest see" << endl;
+	//	if (!is_vcs_initialized(path)) {
+//	init_vcs(path);
+	  //	}
+	//call_status(path, getStagedFileFromRootDic(path));
+	
 
 	return 0;
 }
